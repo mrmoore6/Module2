8268e560787f9579f4911e12c1faec65c3db5257
mrmoore6 <mrmoore@dmacc.edu>
2020-09-05 17:57:49 -0500

    Hello

---
+++ main/camper_age_input.py
@@ -0,0 +1,13 @@
+
+def convert_to_months(year):
+    months = year * 12
+    return(months)
+
+
+if __name__ == '__main__':
+    age_in_months = int(input("Enter years"))
+    age_in_years = convert_to_months(age_in_months)
+    print(age_in_years, "months")
+
+
+
