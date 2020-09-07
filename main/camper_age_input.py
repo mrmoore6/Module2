e2891287ac88d3c433bbdda34be8eb1be1ab6a5e
mrmoore6 <mrmoore@dmacc.edu>
2020-09-07 10:14:26 -0500

    Changed

--- main/camper_age_input.py
+++ main/camper_age_input.py
@@ -5,15 +5,16 @@ Last date modified: 9/5/2020
 
  The purpose of this program is to convert years into months.
 """
+import constants
 def convert_to_months(year):
-    months = year * 12
+    months = year * constants.MONTHS
     return(months)
 
 
 if __name__ == '__main__':
-    age_in_months = int(input("Enter years"))
-    age_in_years = convert_to_months(age_in_months)
-    print(age_in_years, "months")
+    age_in_years = int(input("Enter years"))
+    age_in_months = convert_to_months(age_in_years)
+    print("{} Year is {} months".format(age_in_years, age_in_months))
 
 
 
--- test/test_functions.py
+++ test/test_functions.py
@@ -3,7 +3,7 @@ from main import camper_age_input
 
 class MyTestCase(unittest.TestCase):
     def test_months(self):
-        self.assertEqual(55, camper_age_input.convert_to_months(5))
+        self.assertEqual(54, camper_age_input.convert_to_months(7))
 
 
 if __name__ == '__main__':
